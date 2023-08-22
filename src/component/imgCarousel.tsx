082d279ba237f0344f188a288e2ba017f5323870
abandon888 <1876746719@qq.com>
2023-08-22 16:54:49 +0800

    fix: complete video show

--- src/component/imgCarousel.tsx
+++ src/component/imgCarousel.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Modal } from 'antd'
+import { Button, Modal } from 'antd'
 
 interface ImageViewProps {
   images: string[]
@@ -18,11 +18,39 @@ const ImageView = ({ images }: ImageViewProps) => {
 
   return (
     <>
-      <img src={images[0]} onClick={showModal} />
+      <div
+        style={{
+          width: '80%',
+        }}>
+        <img
+          src={images[0]}
+          onClick={showModal}
+          style={{
+            width: '100%',
+          }}
+        />
 
-      <Modal open={visible} onCancel={handleCancel}>
-        <img src={images[1]} />
-      </Modal>
+        <Modal
+          open={visible}
+          onCancel={handleCancel}
+          closable={false}
+          footer={[
+            <Button
+              onClick={() => {
+                setVisible(false)
+              }}
+              type="primary">
+              关闭
+            </Button>,
+          ]}>
+          <img
+            src={images[1]}
+            style={{
+              width: '100%',
+            }}
+          />
+        </Modal>
+      </div>
     </>
   )
 }
--- src/page/sliceAnalyzeForm.tsx
+++ src/page/sliceAnalyzeForm.tsx
@@ -7,17 +7,49 @@ import ana3 from '../assets/ana3.png'
 import ana3_1 from '../assets/ana3-1.png'
 import ana4 from '../assets/ana4.png'
 import ana4_1 from '../assets/ana4-1.png'
-import { Carousel } from 'antd'
+import { Button, Card, Carousel, ConfigProvider } from 'antd'
+import { useRef } from 'react'
+import { CarouselRef } from 'antd/es/carousel'
 
 export default function SliceAnalyzeForm() {
+  const sliderRef = useRef<CarouselRef>(null)
+
+  const handleNext = (): void => {
+    sliderRef.current?.next()
+  }
+
+  const handlePrev = (): void => {
+    sliderRef.current?.prev()
+  }
+
   return (
     <>
-      <Carousel>
-        <ImageView images={[ana1, ana1_1]} />
-        <ImageView images={[ana2, ana2_1]} />
-        <ImageView images={[ana3, ana3_1]} />
-        <ImageView images={[ana4, ana4_1]} />
-      </Carousel>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorBgContainer: 'blue',
+          },
+        }}>
+        <Carousel dots={true} ref={sliderRef}>
+          <ImageView images={[ana1, ana1_1]} />
+          <ImageView images={[ana2, ana2_1]} />
+          <ImageView images={[ana3, ana3_1]} />
+          <ImageView images={[ana4, ana4_1]} />
+        </Carousel>
+      </ConfigProvider>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          margin: '0 50px',
+        }}>
+        <Button type="primary" onClick={handlePrev} size="large">
+          上一张
+        </Button>
+        <Button type="primary" onClick={handleNext} size="large">
+          下一张
+        </Button>
+      </div>
     </>
   )
 }
